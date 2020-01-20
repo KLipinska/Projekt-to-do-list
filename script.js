8a576cfc3d21afd420d16597968cd708f13a5ae3
Kasia Lipinska <kasialipinska@kasias-macbook-pro.home>
2020-01-20 16:19:15 +0100

    Add and Edit

--- script.js
+++ script.js
@@ -1,210 +1,124 @@
-let $modal, $list, $todoInput, $modalClose, $popupInput, $okModalButton, $addForm, $activeListElement, $dateElement, currentId;
+let $approveBtn, $editInput, $modal, $list;
 
 const BASE_URL = "http://195.181.210.249:3000/todo/";
 
 function main() {
-    prepareDOMElements ();
-    prepareDOMEvents();
-    prepareDate();
+  prepareDOMElements();
+  prepareDOMEvents();
+  getTodos();
 }
 
-function prepareDOMElements () {
-    // $approveBtn = document.getElementById("editToDo");
-    $todoInput = document.getElementById("todoInput");
-    // $editInput = document.getElementById("popupInput");
-    $modal = document.getElementById ("modal");
-    // $approveBtn.addEventListener("click", approveBtnClickHandler);
-    $list = document.getElementById("list");
-    $modalClose = document.getElementsByClassName("close");
-    $popupInput = document.getElementById("popupInput");
-    $okModalButton = document.getElementById("btn_done");
-    $addForm = document.getElementById("addForm");
-    $dateElement = document.getElementById("date");
+function prepareDOMElements() {
+  $approveBtn = document.getElementById("editTodo");
+  $editInput = document.getElementById("popupInput");
+  $modal = document.getElementById("modal");
+  $approveBtn.addEventListener("click", approveBtnClickHandler);
+  $list = document.getElementById("list");
 }
 
-function prepareDate() {
-  const options = { weekday: "long", month: "long", day: "numeric" };
-  const today = new Date();
-  $dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+function prepareDOMEvents() {
+  $list.addEventListener("click", listClickHandler);
+  const $cancelButton = document.getElementById("cancelChanges");
+  const $closeButton = document.getElementById("closePopup");
+  $cancelButton.addEventListener("click", closePopup);
+  $closeButton.addEventListener("click", closePopup);
 }
 
-function prepareDOMEvents () {
-    $list.addEventListener("click", listClickHandler);
-    $addForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const inputValue = $todoInput.value;
-        if (inputValue != "") {
-          postTodos(inputValue);
-          $todoInput.value = ""
-        }
-        else {
-          console.log("nie doda się")
-        }
-      });
-}
-
-// $modalClose[0].addEventListener('click', () => {
-//     $modal.style.display = "none"
-//   });
-// $modalClose[2].addEventListener('click', () => {
-//     $modal.style.display = "none"
-//   });
-
-function openPopup () {
-  $modal.style.display = "block";
-}
-function closePopup () {
-  $modal.style.display = "none";
-}
-// $okModalButton.addEventListener('click', () => {
-//     console.log($popupInput.value);
-//     editTodos();
-//     $modal.style.display = "none"
-//   });
-
-    
-    // const $cancelButton = document.getElementById("cancelChanges");
-    // const $closeButton = document.getElementById("closePopup");
-    // $cancelButton.addEventListener("click", closePopup);
-    // $closeButton.addEventListener("click", closePopup);
-// }
-
-// async function getToDos() {
-//     try {
-//         const response = await axios.get(BASE_URL);
-//         response.data.forEach(addElementToList)
-//     } catch(err) {
-//         console.log("Błąd z serwera");
-//     }
-// }
-
-function getTodos() {
-    $list.innerHTML = "";
-    axios.get(BASE_URL).then((response) => {
-      if (response.status === 200) {
-        console.log(response);
-        response.data.forEach((element) => {
-          addNewElementToList(element.title, element.id, element.extra);
-        })
-      }
-    });
-  }  
-
-  function postTodos() {
-    $list.innerHTML = "";
-    axios.post(BASE_URL, {
-      title: $todoInput.value
-    })
-      .then((response) => {
-        console.log(response);
-        if (response.status === 200) {
-          getTodos();
-        }
-      });
-  }  
-
-  function editTodos() {
-    $list.innerHTML = "";
-    axios.put(BASE_URL + currentId, {
-      title: $popupInput.value
-    })
-      .then((response) => {
-        if (response.status === 200) {
-          getTodos();
-        }
-      });
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+  alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
   }
-  
-  function createElement(title, id, extra) {
-    const newElement = document.createElement('li');
-    newElement.classList.add('flex-row')
-    newElement.innerHTML = "<span class= 'to-left'>" + title + "</span>" + " <div class= 'to-right'><button class='edit'>edit</button>"
-      + " <button class='delete'>delete</button>" + " <button class='done'>mark as done</button></div>";
-    newElement.dataset.id = id;
-    if(extra) {
-      newElement.classList.add('checked')
-      }
-    return newElement;
+  document.getElementById("myInput").value = "";
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
   }
-  
-  function addNewElementToList(title, id, extra) {
-    const newElement = createElement(title, id, extra);
-    $list.appendChild(newElement);
+}
+
+function addElementToList(item) {
+  // const newLi = document.createElement("li");
+  // newLi.classList.add("todo");
+  // newLi.id = "todo-" + item.id;
+  // newLi.innerText = item.title;
+
+  const editBtn = document.createElement("button");
+  editBtn.dataset.id= "edit-" + item.id;
+  editBtn.style.float= "right";
+  editBtn.innerText = "Edit";
+
+  newLi.appendChild(editBtn);
+
+  $list.appendChild(newLi);
+}
+
+async function getToDos() {
+  try {
+      const response = await axios.get(BASE_URL);
+      response.data.forEach(addElementToList)
+  } catch(err) {
+      console.log("Błąd z serwera");
   }
-  
+}
 
-// function addElementToList(item) {
-//     const newLi = document.createElement("li");
-//     newLi.classList.add("todo");
-//     newLi.id = "todo-" + item.id;
-//     newLi.innerText = item.title;
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 
-//     const editBtn = document.createElement("button");
-//     editBtn.dataset.id = "edit-" + item.id;
-//     editBtn.style.float = "right";
-//     editBtn.innerText = "Edit";
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+  var div = this.parentElement;
+  div.style.display = "none";
+  }
+}
 
-//     const deleteBtn = document.createElement("button");
-//     deleteBtn.dataset.id = "delete-" + item.id;
-//     deleteBtn.style.float = "right";
-//     deleteBtn.innerText = "Delete";
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+  ev.target.classList.toggle('checked');
+  }
+}, false);
 
-//     newLi.appendChild(editBtn);
-//     newLi.appendChild(deleteBtn);
 
-//     $list.appendChild(newLi);
-// }
+function removeAll(){
+  var lst = document.getElementsByTagName("ul");
+    lst[0].innerHTML = "";
+}
 
 function listClickHandler(event) {
-    const selectedLi = event.target.parentElement.parentElement;
-    const selectedLabel = event.target.parentElement.parentElement.firstChild;
-        if (event.target.className === 'edit') {
-        console.log("edit");
-        console.log(selectedLabel.innerText);
-        const todoText = selectedLabel.innerText;
-        currentId = selectedLi.dataset.id;
-        $activeListElement = selectedLabel;
-        $popupInput.value = todoText;
-        $modal.style.display = 'block'
-    }
-
-    else if (event.target.className === 'delete') {
-        console.log(selectedLi.dataset.id);
-        removeListElement(selectedLi.dataset.id);
+  if (event.target.tagName === "BUTTON") {
+      const [action, id] = event.target.dataset.id.split("-");
+      if(action === "edit") {
+          $editInput.value = event.target.parentElement.firstChild.textContent.trim();
+          $approveBtn.dataset.editId = id;
+          openPopup();
       }
-      else if (event.target.className === 'done') {
-        console.log("done");
-        markElementAsDone(selectedLi.dataset.id);
-      }
-      else {
-        console.log("not edit")
-      }  
-    }
-
-//     if(event.target.tagName === "BUTTON") {
-//         const [action, id] = event.target.dataset.id.split("-");
-//         if (action === "edit") {
-//             $editInput.value = event.target.parentElement.firstChild.textContent.trim();
-//             $approveBtn.dataset.editId = id;
-//             openPopup();
-//         }
-//     }
-// }
-
-function removeListElement(id) {
-    axios.delete(BASE_URL + id).then((response) => {
-      if (response.status === 200) {
-        getTodos();
-      }
-    })
   }
-  
-function editListElement(element) {
-    const newTodo = prompt("podaj nową nazwę todo")
-    console.log(newTodo)
-    element.innerText = newTodo;
-  }
-  
+}
 
 async function approveBtnClickHandler(event) {
         try {
@@ -214,26 +128,16 @@ async function approveBtnClickHandler(event) {
         } catch(err) {
             console.log(err)
         }
-
         $approveBtn.dataset.editId = undefined;
         $editInput.value = "";
         closePopup();
 }
 
-function markeElementAsDone(id) {
-    axios.put(BASE_URL + id, {
-        extra: true
-    }).then(() => {
-        getTodos();
-    })
+function openPopup () {
+  $modal.style.display = "block";
 }
-
-// function openPopup () {
-//     $modal.style.display = "block";
-// }
-
-// function closePopup () {
-//     $modal.style.display = "none";
-// }
-
+function closePopup () {
+  $modal.style.display = "none";
+}
+    
 document.addEventListener("DOMContentLoaded", main)
